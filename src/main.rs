56999e531e85f3c2c440d8c36f27dbb140c1e8e5
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2022-10-28 22:59:28 +0900

    Export $MESSYENVROOT and $MESSYENVLOCAL

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
+use nix::libc;
 use nix::unistd;
+use std::env;
 use std::error::Error;
 use std::ffi::CString;
+use std::os::unix::ffi::OsStrExt;
 
 fn main() -> Result<(), Box<dyn Error>> {
     use unistd::ForkResult;
     match unsafe { unistd::fork() }? {
-        ForkResult::Child => {
-            let cmd = ["bash"]
-                .iter()
-                .map(|s| CString::new(s.to_string()).unwrap())
-                .collect::<Vec<_>>();
-            let err = unistd::execvp(&cmd[0], &cmd).unwrap_err();
-            println!("messyenv: {}", err.to_string());
-            std::process::exit(1);
-        }
+        ForkResult::Child => exec_shell(),
         ForkResult::Parent { child: _ } => {
             use nix::sys::wait::WaitStatus::*;
             match nix::sys::wait::wait()? {
@@ -24,4 +19,30 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
     }
+}
+
+fn exec_shell() -> Result<(), Box<dyn Error>> {
+    unsafe {
+        let mut dir = env::current_dir()?;
+        dir.push(".messyenv");
+        libc::setenv(
+            CString::new("MESSYENVROOT")?.as_ptr(),
+            CString::new(dir.clone().into_os_string().as_bytes())?.as_ptr(),
+            1,
+        );
+
+        dir.push("local");
+        libc::setenv(
+            CString::new("MESSYENVLOCAL")?.as_ptr(),
+            CString::new(dir.into_os_string().as_bytes())?.as_ptr(),
+            1,
+        );
+    }
+    let cmd = ["bash"]
+        .iter()
+        .map(|s| CString::new(s.to_string()).unwrap())
+        .collect::<Vec<_>>();
+    let err = unistd::execvp(&cmd[0], &cmd).unwrap_err();
+    println!("messyenv: {}", err.to_string());
+    std::process::exit(1);
 }
